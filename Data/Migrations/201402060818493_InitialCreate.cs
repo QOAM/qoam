d88e7cf1543ba57d55605bcb9bfb85df25c8290b
Erik Schierboom <erik_schierboom@hotmail.com>
2014-02-06 09:23:49 +0100

    Fixed problem with migrations. Fixes #17

---
+++ Data/Migrations/201402060818493_InitialCreate.cs
@@ -0,0 +1,16 @@
+namespace RU.Uci.OAMarket.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
