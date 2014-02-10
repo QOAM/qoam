2f9850fde9fabe4163f51ee817753e653e99a476
Erik Schierboom <erik_schierboom@hotmail.com>
2014-02-10 12:57:29 +0100

    Allow parsing of alternate editions from Ulrichs. Fixes #16

--- Domain.Tests/Import/UlrichsImportTests.cs
+++ Domain.Tests/Import/UlrichsImportTests.cs
@@ -1,5 +1,9 @@
 ﻿namespace RU.Uci.OAMarket.Domain.Tests.Import
 {
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Xml.Linq;
+
     using RU.Uci.OAMarket.Domain.Import;
     using RU.Uci.OAMarket.Domain.Tests.Import.Resources;
 
@@ -8,7 +12,7 @@
     public class UlrichsImportTests
     {
         [Fact]
-        public void ParseJournalsWithJournalTypeIsAllWillAllParseJournalsFromXml()
+        public void ParseJournalsWithJournalTypeIsAllAndXmlWithoutAlternateEditionsWillParseAllJournalsFromXml()
         {
             // Arrange
             var ulrichsSettings = new UlrichsSettings();
@@ -20,9 +24,23 @@
             // Assert
             Assert.Equal(281, journals.Count);
         }
-        
+
         [Fact]
-        public void ParseJournalsWithJournalTypeIsOpenAccessWillAllParseOnlyOpenAccessJournalsFromXml()
+        public void ParseJournalsWithJournalTypeIsAllAndXmlWithAlternateEditionsWillParseAllJournalsAndAlternateEditionsFromXml()
+        {
+            // Arrange
+            var ulrichsSettings = new UlrichsSettings();
+            var ulrichsImport = new UlrichsImport(new UlrichsClient(ulrichsSettings), new UlrichsCache(ulrichsSettings));
+
+            // Act
+            var journals = ulrichsImport.ParseJournals(GetUlrichsXmlWithAlternateEditions(), UlrichsImport.UlrichsJournalType.All);
+
+            // Assert
+            Assert.Equal(483, journals.Count);
+        }
+
+        [Fact]
+        public void ParseJournalsWithJournalTypeIsOpenAccessAndXmlWithoutAlternateEditionsWillParseOnlyOpenAccessJournalsFromXml()
         {
             // Arrange
             var ulrichsSettings = new UlrichsSettings();
@@ -35,9 +53,159 @@
             Assert.Equal(77, journals.Count);
         }
 
+        [Fact]
+        public void ParseJournalsWithJournalTypeIsOpenAccessAndXmlWithAlternateEditionsWillParseOnlyOpenAccessJournalsFromXml()
+        {
+            // Arrange
+            var ulrichsSettings = new UlrichsSettings();
+            var ulrichsImport = new UlrichsImport(new UlrichsClient(ulrichsSettings), new UlrichsCache(ulrichsSettings));
+
+            // Act
+            var journals = ulrichsImport.ParseJournals(GetUlrichsXmlWithAlternateEditions(), UlrichsImport.UlrichsJournalType.OpenAccess);
+
+            // Assert
+            Assert.Equal(123, journals.Count);
+        }
+
+        [Fact]
+        public void ParseJournalWithJournalWithoutAlternateEditionsReturnsCorrectlyParsedJournal()
+        {
+            // Arrange
+            var ulrichsSettings = new UlrichsSettings();
+            var ulrichsImport = new UlrichsImport(new UlrichsClient(ulrichsSettings), new UlrichsCache(ulrichsSettings));
+
+            // Act
+            var journals = ulrichsImport.ParseJournal(XElement.Parse(GetUlrichsJournalXmlWithoutAlternateEditions()));
+
+            // Assert
+            AssertEqualJournal(GetExpectedJournal(), journals.First());
+        }
+
+        [Fact]
+        public void ParseJournalWithJournalWithoutAlternateEditionsReturnsOneJournal()
+        {
+            // Arrange
+            var ulrichsSettings = new UlrichsSettings();
+            var ulrichsImport = new UlrichsImport(new UlrichsClient(ulrichsSettings), new UlrichsCache(ulrichsSettings));
+
+            // Act
+            var journals = ulrichsImport.ParseJournal(XElement.Parse(GetUlrichsJournalXmlWithoutAlternateEditions()));
+
+            // Assert
+            Assert.Equal(1, journals.Count());
+        }
+
+        [Fact]
+        public void ParseJournalWithJournalWithAlternateEditionsReturnsCorrectlyParsedJournal()
+        {
+            // Arrange
+            var ulrichsSettings = new UlrichsSettings();
+            var ulrichsImport = new UlrichsImport(new UlrichsClient(ulrichsSettings), new UlrichsCache(ulrichsSettings));
+
+            // Act
+            var journals = ulrichsImport.ParseJournal(XElement.Parse(GetUlrichsJournalXmlWithAlternateEditions()));
+
+            // Assert
+            AssertEqualJournal(GetExpectedJournal(), journals.First());
+        }
+
+        [Fact]
+        public void ParseJournalWithJournalWithAlternateEditionsReturnsMultipleJournals()
+        {
+            // Arrange
+            var ulrichsSettings = new UlrichsSettings();
+            var ulrichsImport = new UlrichsImport(new UlrichsClient(ulrichsSettings), new UlrichsCache(ulrichsSettings));
+
+            // Act
+            var journals = ulrichsImport.ParseJournal(XElement.Parse(GetUlrichsJournalXmlWithAlternateEditions()));
+
+            // Assert
+            Assert.Equal(2, journals.Count());
+        }
+
+        [Fact]
+        public void ParseJournalWithJournalWithCeasedAlternateEditionReturnsOneJournal()
+        {
+            // Arrange
+            var ulrichsSettings = new UlrichsSettings();
+            var ulrichsImport = new UlrichsImport(new UlrichsClient(ulrichsSettings), new UlrichsCache(ulrichsSettings));
+
+            // Act
+            var journals = ulrichsImport.ParseJournal(XElement.Parse(GetUlrichsJournalXmlWithCeasedAlternateEdition()));
+
+            // Assert
+            Assert.Equal(1, journals.Count());
+            AssertEqualJournal(GetExpectedJournal(), journals.First());
+        }
+
+        [Fact]
+        public void ParseJournalWithJournalWithoutPublisherReturnsJournalWithDefaultPublisherName()
+        {
+            // Arrange
+            var ulrichsSettings = new UlrichsSettings();
+            var ulrichsImport = new UlrichsImport(new UlrichsClient(ulrichsSettings), new UlrichsCache(ulrichsSettings));
+
+            // Act
+            var journals = ulrichsImport.ParseJournal(XElement.Parse(GetUlrichsJournalXmlWithoutPublisher()));
+
+            // Assert
+            var parsedJournal = journals.First();
+            Assert.Equal("<none indicated>", parsedJournal.Publisher.Name);
+        }
+
+        private static Journal GetExpectedJournal()
+        {
+            return new Journal
+                   {
+                       Title = "Postepy Rehabilitacji",
+                       ISSN = "0860-6161",
+                       Link = "http://www.asla.org.au/pubs/access/access.htm",
+                       Publisher = new Publisher { Name = "L E D S Publishing Co. Inc." },
+                       Country = new Country { Name = "Poland" },
+                       Languages = new List<Language> { new Language { Name = "English" }, new Language { Name = "Polish" } },
+                       Subjects = new List<Subject> { new Subject { Name = "medical sciences-physical medicine and rehabilitation" } },
+                   };
+        }
+
+        private static void AssertEqualJournal(Journal expectedJournal, Journal parsedJournal)
+        {
+            Assert.Equal(expectedJournal.Title, parsedJournal.Title);
+            Assert.Equal(expectedJournal.ISSN, parsedJournal.ISSN);
+            Assert.Equal(expectedJournal.Link, parsedJournal.Link);
+            Assert.Equal(expectedJournal.Publisher.Name, parsedJournal.Publisher.Name);
+            Assert.Equal(expectedJournal.Country.Name, parsedJournal.Country.Name);
+            Assert.Equal(expectedJournal.Languages.Select(l => l.Name), parsedJournal.Languages.Select(l => l.Name));
+            Assert.Equal(expectedJournal.Subjects.Select(s => s.Name), parsedJournal.Subjects.Select(s => s.Name));
+        }
+
         private static string GetUlrichsXml()
         {
             return ResourceReader.GetContentsOfResource("ulrichs.xml");
+        }
+
+        private static string GetUlrichsXmlWithAlternateEditions()
+        {
+            return ResourceReader.GetContentsOfResource("ulrichswithalternateeditions.xml");
+        }
+
+        private static string GetUlrichsJournalXmlWithoutAlternateEditions()
+        {
+            return ResourceReader.GetContentsOfResource("ulrichsjournalwithoutalternateeditions.xml");
+        }
+
+        private static string GetUlrichsJournalXmlWithAlternateEditions()
+        {
+            return ResourceReader.GetContentsOfResource("ulrichsjournalwithalternateeditions.xml");
+        }
+
+        private static string GetUlrichsJournalXmlWithCeasedAlternateEdition()
+        {
+            return ResourceReader.GetContentsOfResource("ulrichsjournalwithceasedalternateedition.xml");
+        }
+
+        private static string GetUlrichsJournalXmlWithoutPublisher()
+        {
+            return ResourceReader.GetContentsOfResource("ulrichsjournalwithoutpublisher.xml");
         }
     }
 }
--- Domain/Import/UlrichsImport.cs
+++ Domain/Import/UlrichsImport.cs
@@ -9,17 +9,11 @@
 
     public class UlrichsImport
     {
-        public enum UlrichsJournalType
-        {
-            All = 0,
-            OpenAccess
-        }
-
         private const string MissingPublisherName = "<none indicated>";
 
         private readonly UlrichsClient ulrichsClient;
         private readonly UlrichsCache ulrichsCache;
-        
+
         public UlrichsImport(UlrichsClient ulrichsClient, UlrichsCache ulrichsCache)
         {
             Requires.NotNull(ulrichsClient, "ulrichsClient");
@@ -29,27 +23,10 @@
             this.ulrichsCache = ulrichsCache;
         }
 
-        private void DownloadJournals()
+        public enum UlrichsJournalType
         {
-            if (!this.ulrichsClient.TryLogOn())
-            {
-                throw new InvalidOperationException("Could not log-in to Ulrichs.");
-            }
-
-            if (!this.ulrichsClient.RequestReportToken())
-            {
-                throw new InvalidOperationException("Could not request report token.");
-            }
-            
-            for (var page = 1; page <= this.ulrichsClient.NumberOfPages; ++page)
-            {
-                if (this.ulrichsCache.HasExpired(page))
-                {
-                    this.ulrichsCache.Add(this.DownloadJournalsForPage(page), page);    
-                }
-            }
-
-            this.ulrichsClient.LogOff();
+            All = 0,
+            OpenAccess
         }
 
         public IList<Journal> GetJournals(UlrichsJournalType journalType)
@@ -57,14 +34,9 @@
             this.DownloadJournals();
 
             var journals = new List<Journal>();
-            
+
             foreach (var journalsXml in this.ulrichsCache.GetAll())
             {
-                if (journalsXml.Contains("1466-8033"))
-                {
-                    Console.WriteLine();
-                }
-
                 journals.AddRange(this.ParseJournals(journalsXml, journalType));
             }
 
@@ -73,24 +45,57 @@
 
         public IList<Journal> ParseJournals(string xml, UlrichsJournalType journalType)
         {
-            var journalElements = XDocument.Parse(xml).Descendants("UlrichsDataRecords").Descendants("Record");
-            journalElements = journalType == UlrichsJournalType.All ? journalElements.Where(IsJournal) : journalElements.Where(IsOpenAccessJournal);
+            var ulrichsElements = XDocument.Parse(xml).Descendants("UlrichsDataRecords").Descendants("Record");
+            var journalElements = journalType == UlrichsJournalType.All ? ulrichsElements.Where(IsJournal) : ulrichsElements.Where(IsOpenAccessJournal);
 
-            return journalElements.Select(this.ParseJournal).ToList();
+            return journalElements.SelectMany(this.ParseJournal).ToList();
         }
 
-        private Journal ParseJournal(XElement recordElement)
+        public IEnumerable<Journal> ParseJournal(XElement recordElement)
         {
-            return new Journal
-                   {
-                       Title = recordElement.Element("Title").Value, 
-                       ISSN = recordElement.Element("ISSN").Value, 
-                       Link = recordElement.Element("JournalWebsiteURL").Value, 
-                       Publisher = ParsePublisher(recordElement), 
-                       Country = ParseCountry(recordElement), 
-                       Languages = this.ParseLanguages(recordElement), 
-                       Subjects = this.ParseSubjects(recordElement)
-                   };
+            var regularJournal = new Journal
+                                 {
+                                     Title = recordElement.Element("Title").Value,
+                                     ISSN = recordElement.Element("ISSN").Value,
+                                     Link = recordElement.Element("JournalWebsiteURL").Value,
+                                     Publisher = ParsePublisher(recordElement),
+                                     Country = ParseCountry(recordElement),
+                                     Languages = this.ParseLanguages(recordElement),
+                                     Subjects = this.ParseSubjects(recordElement)
+                                 };
+
+            yield return regularJournal;
+
+            foreach (var alternateEditionElement in GetAlternateEditionElements(recordElement))
+            {
+                yield return new Journal
+                             {
+                                 Title = regularJournal.Title,
+                                 ISSN = alternateEditionElement.Element("ISSN").Value,
+                                 Link = regularJournal.Link,
+                                 Publisher = ParsePublisher(recordElement),
+                                 Country = ParseCountry(recordElement),
+                                 Languages = this.ParseLanguages(recordElement),
+                                 Subjects = this.ParseSubjects(recordElement)
+                             };
+            }
+        }
+
+        private static IEnumerable<XElement> GetAlternateEditionElements(XElement recordElement)
+        {
+            return recordElement.Descendants("AlternateEdition").Where(IsAlternateEdition);
+        }
+
+        private static bool IsAlternateEdition(XElement alternateEditionElement)
+        {
+            var statusElement = alternateEditionElement.Element("Status");
+
+            if (statusElement != null && statusElement.Value == "Ceased")
+            {
+                return false;
+            }
+
+            return alternateEditionElement.Element("ISSN") != null;
         }
 
         private static Publisher ParsePublisher(XElement recordElement)
@@ -110,33 +115,9 @@
             return new Country { Name = recordElement.Element("CountryCode").Element("Name").Value.Trim() };
         }
 
-        private IList<Language> ParseLanguages(XElement recordElement)
-        {
-            var languagesElement = recordElement.Element("LanguageSet");
-
-            if (languagesElement == null)
-            {
-                return new List<Language>();
-            }
-
-            return languagesElement.Elements("Language").Select(ParseLanguage).ToList();
-        }
-
         private static Language ParseLanguage(XElement languageElement)
         {
             return new Language { Name = languageElement.Element("Language").Value.Trim() };
-        }
-
-        private IList<Subject> ParseSubjects(XElement recordElement)
-        {
-            var subjectsElement = recordElement.Element("SubjectSet");
-
-            if (subjectsElement == null)
-            {
-                return new List<Subject>();
-            }
-
-            return subjectsElement.Elements("Subject").Select(ParseSubject).ToList();
         }
 
         private static Subject ParseSubject(XElement subjectElement)
@@ -146,10 +127,10 @@
 
         private static bool IsJournal(XElement journalElement)
         {
-            if (journalElement.Element("Title") == null || 
-                journalElement.Element("ISSN") == null || 
-                journalElement.Element("JournalWebsiteURL") == null || 
-                journalElement.Element("DocumentType") == null || 
+            if (journalElement.Element("Title") == null ||
+                journalElement.Element("ISSN") == null ||
+                journalElement.Element("JournalWebsiteURL") == null ||
+                journalElement.Element("DocumentType") == null ||
                 journalElement.Element("CountryCode") == null)
             {
                 return false;
@@ -180,6 +161,53 @@
             }
 
             return false;
+        }
+
+        private void DownloadJournals()
+        {
+            if (!this.ulrichsClient.TryLogOn())
+            {
+                throw new InvalidOperationException("Could not log-in to Ulrichs.");
+            }
+
+            if (!this.ulrichsClient.RequestReportToken())
+            {
+                throw new InvalidOperationException("Could not request report token.");
+            }
+
+            for (var page = 1; page <= this.ulrichsClient.NumberOfPages; ++page)
+            {
+                if (this.ulrichsCache.HasExpired(page))
+                {
+                    this.ulrichsCache.Add(this.DownloadJournalsForPage(page), page);
+                }
+            }
+
+            this.ulrichsClient.LogOff();
+        }
+
+        private IList<Language> ParseLanguages(XElement recordElement)
+        {
+            var languagesElement = recordElement.Element("LanguageSet");
+
+            if (languagesElement == null)
+            {
+                return new List<Language>();
+            }
+
+            return languagesElement.Elements("Language").Select(ParseLanguage).ToList();
+        }
+
+        private IList<Subject> ParseSubjects(XElement recordElement)
+        {
+            var subjectsElement = recordElement.Element("SubjectSet");
+
+            if (subjectsElement == null)
+            {
+                return new List<Subject>();
+            }
+
+            return subjectsElement.Elements("Subject").Select(ParseSubject).ToList();
         }
 
         private string DownloadJournalsForPage(int page)
