241af49a037d88bbc38865ed1b86454a1a3f4d9f
Erik Schierboom <erik_schierboom@hotmail.com>
2014-03-31 14:53:36 +0200

    Some bundling improvements

--- Website/App_Start/BundleConfig.cs
+++ Website/App_Start/BundleConfig.cs
@@ -6,9 +6,9 @@
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/scripts/libraries").Include("~/Scripts/jquery-{version}.js", "~/Scripts/bootstrap.js", "~/Scripts/modernizr-{version}.js"));
+            bundles.Add(new ScriptBundle("~/scripts/libraries").Include("~/Scripts/jquery-{version}.js", "~/Scripts/bootstrap.js", "~/Scripts/modernizr-{version}.js", "~/Scripts/knockout-{version}.js", "~/Scripts/knockout.mapping-latest.js", "~/Scripts/knockout.isdirty.js", "~/Scripts/spin.js"));
             bundles.Add(new ScriptBundle("~/scripts/form").Include("~/Scripts/jquery.validate.js", "~/Scripts/jquery.validate.unobtrusive.js", "~/Scripts/jquery.unobtrusive-ajax.js", "~/Scripts/globalize/globalize.js", "~/Scripts/globalize/cultures/globalize.cultures.js", "~/Scripts/jquery.validate.globalize.js"));
-            bundles.Add(new ScriptBundle("~/scripts/application").Include("~/Scripts/Controllers/*.js", "~/Scripts/Helpers/*.js"));
+            bundles.Add(new ScriptBundle("~/scripts/application").Include("~/Scripts/Helpers/*.js", "~/Scripts/Controllers/*.js"));
             bundles.Add(new ScriptBundle("~/scripts/fancybox").Include("~/Scripts/jquery.ipicture.min.js", "~/Scripts/jquery.fancybox.js"));
             bundles.Add(new ScriptBundle("~/scripts/typeahead").Include("~/Scripts/typeahead.bundle.js"));
             bundles.Add(new ScriptBundle("~/scripts/slider").Include("~/Scripts/bootstrap-slider.js"));
