a0472ac7047eb070ca707f76b1b3eceddb8aac1d
Erik Schierboom <erik_schierboom@hotmail.com>
2014-08-20 11:38:38 +0200

    Login failure page shows message again. Closes #33

--- Website/Models/LoginFailureReason.cs
+++ Website/Models/LoginFailureReason.cs
@@ -1,18 +1,9 @@
 ﻿namespace QOAM.Website.Models
 {
-    using System.ComponentModel.DataAnnotations;
-
-    using QOAM.Website.Resources;
-
     public enum LoginFailureReason
     {
-        [Display(ResourceType = typeof(Errors), Name = "LoginFailureReason_ExternalAuthenticationFailed")]
         ExternalAuthenticationFailed,
-
-        [Display(ResourceType = typeof(Errors), Name = "LoginFailureReason_UsernameAlreadyExists")]
         UsernameAlreadyExists,
-
-        [Display(ResourceType = typeof(Errors), Name = "LoginFailureReason_SaveFailed")]
         SaveFailed,
     }
 }
