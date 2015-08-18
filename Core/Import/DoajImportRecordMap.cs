f1e62bd0bd3b2ad155b5b2f2d81d5dd074e79c30
Erik Schierboom <erik_schierboom@hotmail.com>
2015-08-18 16:04:35 +0200

    Use EISSN before trying ISSN

--- Core/Import/DoajImportRecordMap.cs
+++ Core/Import/DoajImportRecordMap.cs
@@ -15,15 +15,21 @@ namespace QOAM.Core.Import
             Map(m => m.URL).Name("Journal URL");
             Map(m => m.Publisher).Name("Publisher");
             Map(m => m.Language).Name("Full text language");
-            Map(m => m.ISSN).Name("Journal ISSN (print version)");
+            Map(m => m.ISSN).ConvertUsing(MapISSN);
             Map(m => m.Subjects).Name("Keywords");
             Map(m => m.Country).Name("Country of publisher");
-            Map(m => m.HasSeal).ConvertUsing(ParseHasSeal);
+            Map(m => m.HasSeal).ConvertUsing(MapHasSeal);
         }
 
-        private static bool ParseHasSeal(ICsvReaderRow r)
+        private static bool MapHasSeal(ICsvReaderRow r)
         {
             return HasSealTrueValues.Contains(r.GetField("DOAJ Seal") ?? string.Empty);
+        }
+
+        private static string MapISSN(ICsvReaderRow r)
+        {
+            var eissn = r.GetField("Journal EISSN (online version)");
+            return string.IsNullOrWhiteSpace(eissn) ? r.GetField("Journal ISSN (print version)") : eissn;
         }
     }
 }
