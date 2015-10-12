722e423786a947bdd99953f24be55e12388bf89a
Sergi <sergi@codenamed.nl>
2015-10-12 14:00:27 +0200

    Implemented Editing Institutions

---
+++ Website/ViewModels/Institutions/UpsertViewModel.cs
@@ -0,0 +1,31 @@
+﻿namespace QOAM.Website.ViewModels.Institutions
+{
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+
+    using QOAM.Core;
+
+    public class UpsertViewModel
+    {
+        public int? Id { get; set; }
+
+        [Required]
+        [StringLength(1000)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(1000)]
+        [DisplayName("Domainname")]
+        public string ShortName { get; set; }
+
+        public Institution ToInstitution()
+        {
+            return new Institution
+            {
+                Id = Id.GetValueOrDefault(),
+                Name = this.Name, 
+                ShortName = this.ShortName.Replace("http://", string.Empty).Replace("www.", string.Empty),
+            };
+        }
+    }
+}
