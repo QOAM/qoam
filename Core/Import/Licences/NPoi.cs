208afc16856821d8cd0d6ae2f197badb91643806
Sergi <sergi@codenamed.nl>
2015-10-13 22:07:03 +0200

    Case 468: Started digging into NPOI to import Excel files

--- Core/Import/Licences/NPoi.cs
+++ Core/Import/Licences/NPoi.cs
@@ -1,246 +1,248 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.IO;
-//using System.Linq;
-//using System.Net.Mail;
-//using NPOI.SS.UserModel;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+using NPOI.HSSF.Extractor;
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
 
-//namespace QOAM.Core.Import.Licences
-//{
-//    public static class NPoi
-//    {
-//        private static ISheet GetFirstSheet(Stream input)
-//        {
-//            return GetWorkbook(input).GetSheetAt(0);
-//        }
+namespace QOAM.Core.Import.Licences
+{
+    public static class NPoi
+    {
+        public static ISheet GetFirstSheet(Stream input)
+        {
+            return GetWorkbook(input).GetSheetAt(0);
+        }
 
-//        private static IWorkbook GetWorkbook(Stream input)
-//        {
-//            return WorkbookFactory.Create(input);
-//        }
+        public static IWorkbook GetWorkbook(Stream input)
+        {
+            return WorkbookFactory.Create(input);
+        }
 
 
-//        public static IEnumerable<IRow> NonHeaderRows(this ISheet sheet)
-//        {
-//            for (var rowNum = sheet.FirstRowNum + 1; rowNum <= sheet.LastRowNum; ++rowNum)
-//            {
-//                yield return sheet.GetRow(rowNum);
-//            }
-//        }
+        public static IEnumerable<IRow> NonHeaderRows(this ISheet sheet)
+        {
+            for (var rowNum = sheet.FirstRowNum + 1; rowNum <= sheet.LastRowNum; ++rowNum)
+            {
+                yield return sheet.GetRow(rowNum);
+            }
+        }
 
-//        public static string GetCellValueAsString(this IRow row, ImportFileColumn column)
-//        {
-//            if (row == null)
-//            {
-//                return null;
-//            }
+        //public static string GetCellValueAsString(this IRow row, IColumn column)
+        //{
+        //    if (row == null)
+        //    {
+        //        return null;
+        //    }
 
-//            var cell = row.GetCell(column.Index);
+        //    var cell = row.GetCell(column.Index);
 
-//            if (cell == null)
-//            {
-//                return null;
-//            }
+        //    if (cell == null)
+        //    {
+        //        return null;
+        //    }
 
-//            if (cell.CellType == CellType.Numeric)
-//            {
-//                return ((int)cell.NumericCellValue).ToString();
-//            }
+        //    if (cell.CellType == CellType.Numeric)
+        //    {
+        //        return ((int)cell.NumericCellValue).ToString();
+        //    }
 
-//            if (cell.CellType == CellType.String)
-//            {
-//                return cell.StringCellValue.TrimNullSafe().ToNullIfEmpty();
-//            }
+        //    if (cell.CellType == CellType.String)
+        //    {
+        //        return cell.StringCellValue.TrimNullSafe().ToNullIfEmpty();
+        //    }
 
-//            return null;
-//        }
+        //    return null;
+        //}
 
-//        public static bool? GetCellValueAsNullableBoolean(this IRow row, ImportFileColumn column)
-//        {
-//            var value = row.GetCellValueAsString(column);
+        //public static bool? GetCellValueAsNullableBoolean(this IRow row, ImportFileColumn column)
+        //{
+        //    var value = row.GetCellValueAsString(column);
 
-//            if (value == null)
-//            {
-//                return null;
-//            }
+        //    if (value == null)
+        //    {
+        //        return null;
+        //    }
 
-//            if (BooleanTrueValues.Contains(value))
-//            {
-//                return true;
-//            }
+        //    if (BooleanTrueValues.Contains(value))
+        //    {
+        //        return true;
+        //    }
 
-//            if (BooleanFalseValues.Contains(value))
-//            {
-//                return false;
-//            }
+        //    if (BooleanFalseValues.Contains(value))
+        //    {
+        //        return false;
+        //    }
 
-//            return null;
-//        }
+        //    return null;
+        //}
 
-//        public static int? GetCellValueAsNullablePositiveInteger(this IRow row, ImportFileColumn column)
-//        {
-//            if (row == null)
-//            {
-//                return null;
-//            }
+        //public static int? GetCellValueAsNullablePositiveInteger(this IRow row, ImportFileColumn column)
+        //{
+        //    if (row == null)
+        //    {
+        //        return null;
+        //    }
 
-//            var cell = row.GetCell(column.Index);
+        //    var cell = row.GetCell(column.Index);
 
-//            if (cell == null)
-//            {
-//                return null;
-//            }
+        //    if (cell == null)
+        //    {
+        //        return null;
+        //    }
 
-//            if (cell.CellType == CellType.Numeric)
-//            {
-//                return (int)cell.NumericCellValue;
-//            }
+        //    if (cell.CellType == CellType.Numeric)
+        //    {
+        //        return (int)cell.NumericCellValue;
+        //    }
 
-//            if (cell.CellType == CellType.String)
-//            {
-//                if (string.IsNullOrWhiteSpace(cell.StringCellValue))
-//                {
-//                    return null;
-//                }
+        //    if (cell.CellType == CellType.String)
+        //    {
+        //        if (string.IsNullOrWhiteSpace(cell.StringCellValue))
+        //        {
+        //            return null;
+        //        }
 
-//                try
-//                {
-//                    var integerValue = int.Parse(cell.StringCellValue.Trim(), ParseCulture);
+        //        try
+        //        {
+        //            var integerValue = int.Parse(cell.StringCellValue.Trim(), ParseCulture);
 
-//                    if (integerValue < 0)
-//                    {
-//                        throw new RowParseException(row, string.Format(Errors.Parse_InvalidPositiveNumber, row.RowNum, column.Name, integerValue));
-//                    }
+        //            if (integerValue < 0)
+        //            {
+        //                throw new RowParseException(row, string.Format(Errors.Parse_InvalidPositiveNumber, row.RowNum, column.Name, integerValue));
+        //            }
 
-//                    return integerValue;
-//                }
-//                catch (FormatException ex)
-//                {
-//                    throw new RowParseException(row, string.Format(Errors.Parse_InvalidNumber, row.RowNum, column.Name, cell.StringCellValue), ex);
-//                }
-//            }
+        //            return integerValue;
+        //        }
+        //        catch (FormatException ex)
+        //        {
+        //            throw new RowParseException(row, string.Format(Errors.Parse_InvalidNumber, row.RowNum, column.Name, cell.StringCellValue), ex);
+        //        }
+        //    }
 
-//            return null;
-//        }
+        //    return null;
+        //}
 
-//        public static int? GetCellValueAsNullableYear(this IRow row, ImportFileColumn column)
-//        {
-//            var value = row.GetCellValueAsNullablePositiveInteger(column);
+        //public static int? GetCellValueAsNullableYear(this IRow row, ImportFileColumn column)
+        //{
+        //    var value = row.GetCellValueAsNullablePositiveInteger(column);
 
-//            if (value == null)
-//            {
-//                return null;
-//            }
+        //    if (value == null)
+        //    {
+        //        return null;
+        //    }
 
-//            if (value < 1000 || value > 9999)
-//            {
-//                throw new RowParseException(row, string.Format(Errors.Parse_InvalidYear, row.RowNum, column.Name, value));
-//            }
+        //    if (value < 1000 || value > 9999)
+        //    {
+        //        throw new RowParseException(row, string.Format(Errors.Parse_InvalidYear, row.RowNum, column.Name, value));
+        //    }
 
-//            return value;
-//        }
+        //    return value;
+        //}
 
-//        public static DateTime? GetCellValueAsNullableDateTime(this IRow row, ImportFileColumn column)
-//        {
-//            if (row == null)
-//            {
-//                return null;
-//            }
+        //public static DateTime? GetCellValueAsNullableDateTime(this IRow row, ImportFileColumn column)
+        //{
+        //    if (row == null)
+        //    {
+        //        return null;
+        //    }
 
-//            var cell = row.GetCell(column.Index);
+        //    var cell = row.GetCell(column.Index);
 
-//            if (cell == null)
-//            {
-//                return null;
-//            }
+        //    if (cell == null)
+        //    {
+        //        return null;
+        //    }
 
-//            if (cell.CellType == CellType.Numeric)
-//            {
-//                return cell.DateCellValue;
-//            }
+        //    if (cell.CellType == CellType.Numeric)
+        //    {
+        //        return cell.DateCellValue;
+        //    }
 
-//            if (cell.CellType == CellType.String)
-//            {
-//                if (string.IsNullOrWhiteSpace(cell.StringCellValue))
-//                {
-//                    return null;
-//                }
+        //    if (cell.CellType == CellType.String)
+        //    {
+        //        if (string.IsNullOrWhiteSpace(cell.StringCellValue))
+        //        {
+        //            return null;
+        //        }
 
-//                try
-//                {
-//                    return DateTime.Parse(cell.StringCellValue.Trim(), ParseCulture);
-//                }
-//                catch (FormatException ex)
-//                {
-//                    throw new RowParseException(row, string.Format(Errors.Parse_InvalidDateTime, row.RowNum, column.Name, cell.StringCellValue), ex);
-//                }
-//            }
+        //        try
+        //        {
+        //            return DateTime.Parse(cell.StringCellValue.Trim(), ParseCulture);
+        //        }
+        //        catch (FormatException ex)
+        //        {
+        //            throw new RowParseException(row, string.Format(Errors.Parse_InvalidDateTime, row.RowNum, column.Name, cell.StringCellValue), ex);
+        //        }
+        //    }
 
-//            return null;
-//        }
+        //    return null;
+        //}
 
-//        public static Guid? GetCellValueAsNullableGuid(this IRow row, ImportFileColumn column)
-//        {
-//            var value = row.GetCellValueAsString(column);
+        //public static Guid? GetCellValueAsNullableGuid(this IRow row, ImportFileColumn column)
+        //{
+        //    var value = row.GetCellValueAsString(column);
 
-//            if (string.IsNullOrWhiteSpace(value))
-//            {
-//                return null;
-//            }
+        //    if (string.IsNullOrWhiteSpace(value))
+        //    {
+        //        return null;
+        //    }
 
-//            try
-//            {
-//                return Guid.Parse(value.Trim());
-//            }
-//            catch (FormatException ex)
-//            {
-//                throw new RowParseException(row, string.Format(Errors.Parse_InvalidGuid, row.RowNum, column.Name, value), ex);
-//            }
-//        }
+        //    try
+        //    {
+        //        return Guid.Parse(value.Trim());
+        //    }
+        //    catch (FormatException ex)
+        //    {
+        //        throw new RowParseException(row, string.Format(Errors.Parse_InvalidGuid, row.RowNum, column.Name, value), ex);
+        //    }
+        //}
 
-//        public static MailAddress GetCellValueAsMailAddress(this IRow row, ImportFileColumn column)
-//        {
-//            var value = row.GetCellValueAsString(column);
+        //public static MailAddress GetCellValueAsMailAddress(this IRow row, ImportFileColumn column)
+        //{
+        //    var value = row.GetCellValueAsString(column);
 
-//            if (string.IsNullOrWhiteSpace(value))
-//            {
-//                return null;
-//            }
+        //    if (string.IsNullOrWhiteSpace(value))
+        //    {
+        //        return null;
+        //    }
 
-//            try
-//            {
-//                return new MailAddress(value.Trim());
-//            }
-//            catch (FormatException ex)
-//            {
-//                throw new RowParseException(row, string.Format(Errors.Parse_InvalidMailAddress, row.RowNum, column.Name, value), ex);
-//            }
-//        }
+        //    try
+        //    {
+        //        return new MailAddress(value.Trim());
+        //    }
+        //    catch (FormatException ex)
+        //    {
+        //        throw new RowParseException(row, string.Format(Errors.Parse_InvalidMailAddress, row.RowNum, column.Name, value), ex);
+        //    }
+        //}
 
-//        public static bool IsBlank(this IRow row)
-//        {
-//            return row == null || row.Cells == null || row.Cells.All(c => c.IsBlank());
-//        }
+        public static bool IsBlank(this IRow row)
+        {
+            return row?.Cells == null || row.Cells.All(c => c.IsBlank());
+        }
 
-//        public static bool IsBlank(this ICell cell)
-//        {
-//            if (cell == null)
-//            {
-//                return true;
-//            }
+        public static bool IsBlank(this ICell cell)
+        {
+            if (cell == null)
+            {
+                return true;
+            }
 
-//            switch (cell.CellType)
-//            {
-//                case CellType.Unknown:
-//                case CellType.Error:
-//                case CellType.Blank:
-//                    return true;
-//                case CellType.String:
-//                    return cell.RichStringCellValue == null || string.IsNullOrWhiteSpace(cell.RichStringCellValue.String);
-//                default:
-//                    return false;
-//            }
-//        }
+            switch (cell.CellType)
+            {
+                case CellType.Unknown:
+                case CellType.Error:
+                case CellType.Blank:
+                    return true;
+                case CellType.String:
+                    return cell.RichStringCellValue == null || string.IsNullOrWhiteSpace(cell.RichStringCellValue.String);
+                default:
+                    return false;
+            }
+        }
 
-//    }
-//}
+    }
+}
