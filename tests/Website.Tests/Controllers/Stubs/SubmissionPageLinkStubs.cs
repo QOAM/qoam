0606f5301c64e9a6fb2d4f4615cab39af7f90145
Sergi <sergi@codenamed.nl>
2016-03-08 12:09:15 +0100

    Fixed domain matching when Journal Link doesn't have a subdomain (including no www)

--- tests/Website.Tests/Controllers/Stubs/SubmissionPageLinkStubs.cs
+++ tests/Website.Tests/Controllers/Stubs/SubmissionPageLinkStubs.cs
@@ -13,7 +13,7 @@ namespace QOAM.Website.Tests.Controllers.Stubs
                 new SubmissionPageLink { ISSN = "1687-8140", Url = "http://ade.sagepub.com/submission" },
                 new SubmissionPageLink { ISSN = "2073-4395", Url = "http://www.mdpi.com/journal/agronomy/submission" },
                 new SubmissionPageLink { ISSN = "2372-0484", Url = "http://www.aimspress.com/journal/Materials/submission" },
-                new SubmissionPageLink { ISSN = "1687-7675", Url = "http://www.hindawi.com/journals/aess/submission" }
+                new SubmissionPageLink { ISSN = "2050-084X", Url = "http://submit.elifesciences.org/cgi-bin/main.plex" }
             };
 
             return list;
@@ -39,7 +39,7 @@ namespace QOAM.Website.Tests.Controllers.Stubs
                 new Journal { Id = 1, ISSN = "1687-8140", Link = "http://ade.sagepub.com" }, 
                 new Journal { Id = 2, ISSN = "2073-4395", Link = "http://www.mdpi.com/journal/agronomy" },
                 new Journal { Id = 3, ISSN = "2372-0484", Link = "http://www.aimspress.com/journal/Materials" },
-                new Journal { Id = 4, ISSN = "1687-7675", Link = "http://www.hindawi.com/journals/aess" }
+                new Journal { Id = 4, ISSN = "2050-084X", Link = "http://elifesciences.org/" }
             };
         }
     }
