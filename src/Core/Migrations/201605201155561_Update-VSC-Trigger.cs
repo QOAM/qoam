fd8ebb3b21bf2043375effeb4ee9750efaca1b0f
Sergi <sergi@codenamed.nl>
2016-05-20 14:55:49 +0200

    Fixed mix-up on the trigger scripts

--- src/Core/Migrations/201605201155561_Update-VSC-Trigger.cs
+++ src/Core/Migrations/201605201155561_Update-VSC-Trigger.cs
@@ -9,6 +9,7 @@ namespace QOAM.Core.Migrations
         public override void Up()
         {
             Sql("DELETE FROM dbo.__MigrationHistory WHERE [MigrationId] = '201605191108426_Update-VSC-Trigger'");
+            Sql("DELETE FROM dbo.__MigrationHistory WHERE [MigrationId] = '201605201155561_Update-VSC-Trigger'");
             Sql("DROP TRIGGER [ValuationScoreCards.Modified]");
             Sql(ResourceReader.GetContentsOfResource("20160519_Trigger_ValuationScorecards_v2.Modified.sql"));
         }
