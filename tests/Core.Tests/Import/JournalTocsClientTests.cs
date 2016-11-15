d35dea01ce5a323ac82d6cf09c0a2f5a331fc644
Sergi <sergi@codenamed.nl>
2016-11-15 11:53:05 +0100

    Implemented support for the resumption token

--- tests/Core.Tests/Import/JournalTocsClientTests.cs
+++ tests/Core.Tests/Import/JournalTocsClientTests.cs
@@ -1,8 +1,5 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using Moq;
+﻿using Moq;
 using QOAM.Core.Import;
-using QOAM.Core.Repositories;
 using QOAM.Core.Tests.Import.Resources;
 using Xunit;
 
@@ -12,13 +9,41 @@ namespace QOAM.Core.Tests.Import
     {
         Mock<IWebClientFactory> _factory;
         Mock<IWebClient> _webClient;
-        
+        JournalTocsSettings _settings;
+
         [Fact]
         public void DownloadJournals_fetches_a_batch_of_journals()
         {
             var sut = CreateClient();
 
-            _webClient.Setup(x => x.DownloadString(It.IsAny<string>())).Returns(GetJournalTocsFirst500Xml());
+            var journalsXml = GetJournalTocsFirst500Xml();
+
+            _webClient.Setup(x => x.DownloadString($"{_settings.RequestUrl}&resumptionToken=0")).Returns(journalsXml);
+            _webClient.Setup(x => x.DownloadString($"{_settings.RequestUrl}&resumptionToken=1")).Returns(GetJournalTocsNoMoreItemsNotice());
+
+            var result = sut.DownloadJournals();
+
+            Assert.Equal(journalsXml, result);
+        }
+
+        [Fact]
+        public void DownloadJournals_fetches_journals_until_there_are_no_more_batches()
+        {
+            var sut = CreateClient();
+
+            var firstBatch = GetJournalTocsFirst500Xml();
+            var secondBatch = GetJournalTocsNext500Xml();
+
+            _webClient.Setup(x => x.DownloadString($"{_settings.RequestUrl}&resumptionToken=0")).Returns(firstBatch);
+            _webClient.Setup(x => x.DownloadString($"{_settings.RequestUrl}&resumptionToken=1")).Returns(secondBatch);
+            _webClient.Setup(x => x.DownloadString($"{_settings.RequestUrl}&resumptionToken=2")).Returns(GetJournalTocsNoMoreItemsNotice());
+
+            var result = sut.DownloadJournals();
+
+            _webClient.Verify(x => x.DownloadString($"{_settings.RequestUrl}&resumptionToken=0"), Times.Once());
+            _webClient.Verify(x => x.DownloadString(It.IsAny<string>()), Times.Exactly(3));
+
+            Assert.Equal(firstBatch + secondBatch, result);
         }
 
         #region Private Methods
@@ -30,7 +55,8 @@ namespace QOAM.Core.Tests.Import
 
             _factory.Setup(x => x.Create()).Returns(_webClient.Object);
 
-            return new JournalTocsClient(new JournalTocsSettings(), _factory.Object);
+            _settings = new JournalTocsSettings();
+            return new JournalTocsClient(_settings, _factory.Object);
         }
 
         static string GetJournalTocsFirst500Xml()
@@ -38,7 +64,7 @@ namespace QOAM.Core.Tests.Import
             return ResourceReader.GetContentsOfResource("JournalTocs-setup-first-500.xml");
         }
 
-        static string GetJournalTocsNextXml()
+        static string GetJournalTocsNext500Xml()
         {
             return ResourceReader.GetContentsOfResource("JournalTocs-setup-next-500.xml");
         }
