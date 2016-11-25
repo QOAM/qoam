7a83dc18bed67cc88a32fd9a69424e3dea58b561
Sergi <sergi@codenamed.nl>
2016-11-25 20:17:00 +0100

    Created views to import and update JournalTOCs

--- src/Core/Import/JournalsImport.cs
+++ src/Core/Import/JournalsImport.cs
@@ -186,6 +186,7 @@
                     }
                 }
 
+                currentJournal.LastUpdatedOn = DateTime.Now;
                 this.journalRepository.InsertOrUpdate(currentJournal);
             }
 
@@ -201,6 +202,7 @@
                 journal.Languages = journal.Languages.Select(l => languages.First(a => string.Equals(a.Name, l.Name.Trim().ToLowerInvariant(), StringComparison.InvariantCultureIgnoreCase))).ToSet();
                 journal.Subjects = journal.Subjects.Select(s => subjects.First(u => string.Equals(u.Name, s.Name.Trim().ToLowerInvariant(), StringComparison.InvariantCultureIgnoreCase))).ToSet();
                 journal.DateAdded = DateTime.Now;
+                journal.LastUpdatedOn = DateTime.Now;
 
                 this.journalRepository.InsertOrUpdate(journal);
             }
@@ -229,7 +231,10 @@
         private IList<string> GetNewCountryNames(IEnumerable<Journal> journals)
         {
             var currentCountryNames = this.countryRepository.All.Select(c => c.Name.Trim().ToLowerInvariant()).ToSet(StringComparer.InvariantCultureIgnoreCase);
-            var importCountryNames = journals.Select(j => j.Country.Name.Trim().ToLowerInvariant()).ToSet(StringComparer.InvariantCultureIgnoreCase);
+            var importCountryNames = journals.Select(j => j.Country?.Name.Trim().ToLowerInvariant()).Where(c => c != null).ToSet(StringComparer.InvariantCultureIgnoreCase);
+
+            if(!importCountryNames.Any())
+                return new List<string>();
 
             return importCountryNames.Except(currentCountryNames, StringComparer.InvariantCultureIgnoreCase).ToList();
         }
@@ -255,7 +260,7 @@
         private IList<string> GetNewLanguageNames(IEnumerable<Journal> journals)
         {
             var currentLanguageNames = this.languageRepository.All.Select(c => c.Name.ToLowerInvariant()).ToSet(StringComparer.InvariantCultureIgnoreCase);
-            var importLanguageNames = journals.SelectMany(j => j.Languages).Select(l => l.Name.ToLowerInvariant()).ToSet(StringComparer.InvariantCultureIgnoreCase);
+            var importLanguageNames = journals.SelectMany(j => j.Languages).Select(l => l.Name?.ToLowerInvariant()).ToSet(StringComparer.InvariantCultureIgnoreCase);
 
             return importLanguageNames.Except(currentLanguageNames, StringComparer.InvariantCultureIgnoreCase).ToList();
         }
---
+++ src/Website/ViewModels/Import/JournalTocsImportViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+using QOAM.Core.Import;
+
+namespace QOAM.Website.ViewModels.Import
+{
+    public class JournalTocsImportViewModel
+    {
+        [DisplayName("Fetch Mode")]
+        public JournalTocsFetchMode FetchMode { get; set; }
+    }
+}
