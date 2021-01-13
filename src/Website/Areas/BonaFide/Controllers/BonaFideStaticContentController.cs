a633f4c13b68e9f527392bf1f86c2df349e37861
Sergi Papaseit Valls <sergi@codenamed.nl>
2021-01-13 20:40:21 +0100

    Added Why and halfway through adding How text pages

---
+++ src/Website/Areas/BonaFide/Controllers/BonaFideStaticContentController.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Mvc;
+using QOAM.Core.Repositories;
+using QOAM.Website.Controllers;
+using QOAM.Website.Helpers;
+
+namespace QOAM.Website.Areas.BonaFide.Controllers
+{
+    [RouteArea("BonaFide", AreaPrefix = "bfj")]
+    [RoutePrefix("")]
+    [Route("{action}")]
+    public class BonaFideStaticContentController : ApplicationController
+    {
+        // GET
+        public BonaFideStaticContentController(IBaseScoreCardRepository baseScoreCardRepository, IValuationScoreCardRepository valuationScoreCardRepository, IUserProfileRepository userProfileRepository, IAuthentication authentication) : base(baseScoreCardRepository, valuationScoreCardRepository, userProfileRepository, authentication)
+        {
+        }
+
+        [HttpGet, Route("why")]
+        public ActionResult Why()
+        {
+            return View();
+        }
+
+        [HttpGet, Route("how")]
+        public ActionResult How()
+        {
+            return View();
+        }
+    }
+}
