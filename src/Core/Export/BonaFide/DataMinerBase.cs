5e905274a8016237f8b5f83050cc5edc2dc7583f
Sergi Papaseit Valls <sergi@codenamed.nl>
2021-03-28 15:31:52 +0200

    Implemented 4 export formats

---
+++ src/Core/Export/BonaFide/DataMinerBase.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace QOAM.Core.Export.BonaFide
+{
+    public abstract class DataMinerBase : IDataMiner
+    {
+        protected abstract string ContentType { get; }
+        protected abstract string FileNameExtension { get; }
+
+        public DataMiningResult GenerateFile(IList<Journal> journals)
+        {
+            var records = new DataMiningRecord { ISSNs = journals.Select(j => j.ISSN).ToList() };
+
+            var bytes = Serialize(records);
+
+            return new DataMiningResult { Bytes = bytes, ContentType = ContentType, FileNameExtension = FileNameExtension };
+        }
+
+        protected abstract byte[] Serialize(DataMiningRecord record);
+    }
+}
---
+++ src/Core/Export/BonaFide/XmlDataMiner.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+namespace QOAM.Core.Export.BonaFide
+{
+    public class XmlDataMiner : DataMinerBase
+    {
+        protected override string ContentType => "application/xml";
+        protected override string FileNameExtension => "xml";
+
+        protected override byte[] Serialize(DataMiningRecord record)
+        {
+            var serializer = new XmlSerializer(typeof(DataMiningRecord));
+
+            using (var memoryStream = new MemoryStream())
+            {
+                serializer.Serialize(memoryStream, record);
+                memoryStream.Position = 0;
+
+                return memoryStream.ToArray();
+            }
+        }
+    }
+}
